bb28f48b8c7f2e9433ed1eb5ab099878ff80306b
Dani El-Ayyass <dayyass@yandex.ru>
2020-11-20 22:20:20 +0300

    hotfix

--- solutions/problem_002.py
+++ solutions/problem_002.py
@@ -11,7 +11,7 @@ def problem_02(upper_bound: int = 4_000_000):
     result = 0  # cumulative sum
     fib_1 = 0  # prev fib number
     fib_2 = 1  # next fib number
-    while fib_2 <= upper_bound:
+    while fib_1 + fib_2 <= upper_bound:
         fib_1, fib_2 = fib_2, fib_1 + fib_2
         if fib_2 % 2 == 0:
             result += fib_2
