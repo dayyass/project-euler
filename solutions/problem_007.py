a1869f1a20a4ce84a86d41f7a95f2bd60a04b936
Dani El-Ayyass <dayyass@yandex.ru>
2020-11-21 09:35:56 +0300

    add problem 7 solution

---
+++ solutions/problem_007.py
@@ -0,0 +1,20 @@
+from utils import is_prime
+
+
+def problem_007(n_prime: int = 10001) -> int:
+    """
+    - O(1) space-complexity
+
+    Iterate over number, check if number is prime until reach desired prime number.
+    """
+    cnt = 0  # prime numbers counter
+    num = 0  # current number
+    while cnt != n_prime:
+        num += 1
+        if is_prime(num):
+            cnt += 1
+    return num
+
+
+if __name__ == '__main__':
+    print(f'Problem 7 solution: {problem_007()}')
