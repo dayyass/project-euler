e80c34773572804445d570f0e8d5cc8caf2e4dd5
Dani El-Ayyass <dayyass@yandex.ru>
2020-12-13 20:35:41 +0300

    fix problem 16 solution

--- solutions/problem_016.py
+++ solutions/problem_016.py
@@ -1,8 +1,11 @@
+from utils import sum_of_digits_in_number
+
+
 def problem_016(n: int = 1000) -> int:
     """
     Straightforward solution.
     """
-    return sum(int(digit) for digit in str(2**n))
+    return sum_of_digits_in_number(2**n)
 
 
 if __name__ == '__main__':
